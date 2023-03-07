446abb13b9679bc25b9125ac98e6648ece7f92f0
AlexisDrogoul <alexis.drogoul@gmail.com>
2023-03-07 16:07:48 +0700

    Commit of the Icon producer
    Self-contained project for producing GAMA icons

---
+++ icons/ummisco.gama.icons.producer/src/module-info.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+/**
+ * @author drogoul
+ *
+ */
+module ummisco.gama.icons.producer {
+	requires batik.all;
+	requires jdk.xml.dom;
+	requires java.desktop;
+	requires xml.apis.ext;
+	
+}
